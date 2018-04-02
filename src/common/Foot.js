2a14d5edb675282b3f8d14a0e45487e0e035d0c5
HalalChain <34613203+HalalChain@users.noreply.github.com>
2018-04-02 09:52:07 +0800

    Update Foot.js

--- src/common/Foot.js
+++ src/common/Foot.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../style/Foot.css';
-//import Logo from '../images/coin-02.png';
 import { Layout,Row,Col } from 'antd';
 const { Footer } = Layout;
 
@@ -8,11 +7,8 @@ export default class Foot extends Component {
     render() {
         return(
             <div className='footer-wrap'>
-                {/* <Layout> */}
-                    {/* <Footer> */}
                         <p><span>Copyright©️2018 | HLC Foundation Ltd.</span> <span style={{marginLeft:'20px'}}>Contact Email:support@HLC.com</span></p>
-                    {/* </Footer> */}
-                {/* </Layout> */}
+                  
             </div>
         )
     }
